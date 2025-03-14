44e38e0adc3193e1b9636c827ee113ba9de15b90
Zammil00 <muzammilkw07@gmail.com>
2025-03-15 04:13:58 +0700

    Update Theme

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xff42C83C);
+  static const tematerang = Color(0xffF2F2F2);
+  static const temagelap = Color(0xff000000);
+  static const abuabu = Color(0xffBEBEBE);
+  static const abugelap = Color(0xff343434);
+}
---
+++ lib/core/configs/theme/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spotify_app/core/configs/theme/app_colors.dart';
+
+class AppTheme {
+  static final lightTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.tematerang,
+  );
+}
